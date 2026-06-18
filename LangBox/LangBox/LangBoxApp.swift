4950c1122fa1159629c0f64f95abaa0526915bc5
Martin Stanek <stanekmartin@bluewin.ch>
2026-06-18 15:21:33 +0200

    skeleton

---
+++ LangBox/LangBox/LangBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangBoxApp.swift
+//  LangBox
+//
+//  Created by Martin Stanek on 18.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct LangBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
